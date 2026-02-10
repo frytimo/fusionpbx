1a0ac99405ea9d2e7b45369d163a812f6e1c5ab0
Tim Fry <tim@fusionpbx.com>
2026-02-10 01:24:49 +0000

    Initial interface implementation

---
+++ resources/interfaces/database_provider.php
@@ -0,0 +1,23 @@
+<?php
+
+interface database_provider {
+	public function tables(): array;
+	public function table_exists(string $table_name): bool;
+	public function table_info(string $table_name): array;
+	public function column_exists(string $table_name, string $column_name): bool;
+	public function is_connected(): bool;
+	public function execute(string $sql, ?array $parameters = []);
+	public function fetch_row(string $sql, ?array $parameters = []): array;
+	public function fetch_all(string $sql, ?array $parameters = []): array;
+	public function fetch_column(string $sql, ?array $parameters = []): string;
+	public function get_database_indexes(): array;
+	public function connect(): bool;
+	public function begin_transaction(): bool;
+	public function commit(): bool;
+	public function add_transaction($sql, ?array $parameters = []): bool;
+	public function in_transaction(): bool;
+	public function rollback(): bool;
+	public function prepare(string $sql);
+	public function bind_param($statement, $parameter, $value, $data_type = null): bool;
+	public function set_attribute($attribute, $value): bool;
+}
