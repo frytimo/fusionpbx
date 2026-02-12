645c6cb2a764ebb663acaa1c9cf7fc527389b1ff
Tim Fry <tim@fusionpbx.com>
2026-02-12 01:42:06 +0000

    Implement the logout event

---
+++ resources/interfaces/logout_event.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Interface for handling logout events in FusionPBX.
+ * Implementing classes can define actions before and after session destruction.
+ */
+interface logout_event {
+
+	/**
+	 * Executed before the session is destroyed.
+	 * Use this to perform cleanup or logging prior to logout.
+	 */
+	public function on_logout_pre_session_destroy();
+
+	/**
+	 * Executed after the session is destroyed.
+	 * Use this to perform post-logout actions, such as redirects or notifications.
+	 */
+	public function on_logout_post_session_destroy();
+
+}
