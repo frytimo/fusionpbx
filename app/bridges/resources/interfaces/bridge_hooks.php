2e9dcd4bf914654e89d73b7d9bc18b48cd7f29fb
Tim Fry <tim@fusionpbx.com>
2026-03-02 00:06:12 +0000

    Change grammar of class

--- app/bridges/resources/interfaces/bridge_hooks.php
+++ app/bridges/resources/interfaces/bridge_hooks.php
@@ -1,6 +1,6 @@
 <?php
 
-interface bridge_hook extends bridge_list_page_hook, bridge_edit_hook {
+interface bridge_hooks extends bridge_list_page_hook, bridge_edit_hook {
 	// This interface combines both list page hooks and edit page hooks for bridges
 	// Implementing this interface allows a class to hook into both types of pages
 }
